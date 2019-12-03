83363cc28820283b0e54d5f9cee5ef8ff63e7b90
bond-on <tbond77@gmail.com>
2019-12-03 17:55:32 +0100

    Добавил файл page.php
    добавил файл и создал структуру

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
-        <!-- <meta name="viewport" content="initial-scale=1, maximum-scale=1"> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1">
         <meta name="viewport" content="width=350, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Landing Page</title>
-        <?php wp_head(); ?>
+        <!-- <?php wp_head(); ?> -->
     </head>
 <body>
 
